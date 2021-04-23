122066163ebca56139fda90c50fab1c4133baeab
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-04-23 14:07:38 +0200

    Styling should now be as project example

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props) {
+	return (
+		<Card className="expenses">
+			{props.expenses.map((expense) => {
+				return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />;
+			})}
+		</Card>
+	);
+}
+
+export default Expenses;

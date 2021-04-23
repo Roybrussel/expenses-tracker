26b1087655946245297865aabb938c1434fd3c37
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-04-23 13:49:16 +0200

    Created an expenses component that holds ExpenseItem and ExpenseDate as components.

---
+++ src/components/Expenses.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+	return props.expenses.map((expense) => {
+		return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />;
+	});
+}
+
+export default Expenses;
